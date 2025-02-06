79d0ad9ddb1e61cc41013e6ad8575f473c2d0e86
Montazar <github.v0its@slmail.me>
2025-02-06 23:49:41 +0100

    Adds Card and myQvickly as separate payment methods

--- src/templates/payment-categories.php
+++ src/templates/payment-categories.php
@@ -10,32 +10,44 @@ if ( ! empty( $order_id ) ) {
 
 Qvickly_Payments()->session()->get_session( isset( $order ) && ! empty( $order ) ? $order : null );
 
-$payment_categories = Qvickly_Payments()->session()->get_payment_categories();
-if ( ! empty( $payment_categories ) && is_array( $payment_categories ) ) {
-	$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
-	$gateway            = $available_gateways['qvickly_payments'];
-	$chosen_gateway     = $available_gateways[ array_key_first( $available_gateways ) ];
+$payment_categories = array(
+	array(
+		'id'          => 'card',
+		'name'        => __( 'Card', 'qvickly-payments-for-woocommerce' ),
+		'description' => __( 'Pay with card', 'qvickly-payments-for-woocommerce' ),
+		'logo'        => Qvickly_Payments()->gateway()->get_icon(),
+	),
+	array(
+		'id'          => 'myqvickly',
+		'name'        => __( 'myQvickly', 'qvickly-payments-for-woocommerce' ),
+		'description' => __( 'Pay with Qvickly', 'qvickly-payments-for-woocommerce' ),
+		'logo'        => Qvickly_Payments()->gateway()->get_icon(),
+	),
+);
 
-	foreach ( apply_filters( 'qvickly_payments_available_payment_categories', $payment_categories ) as $payment_category ) {
-		$category_id = "qvickly_payments_{$payment_category['type']}";
+$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+$gateway            = $available_gateways['qvickly_payments'];
+$chosen_gateway     = $available_gateways[ array_key_first( $available_gateways ) ];
 
-		$gateway              = $available_gateways['qvickly_payments'] ?? $available_gateways[ $category_id ];
-		$gateway->id          = $category_id;
-		$gateway->icon        = $payment_category['assets']['urls']['logo'] ?? null;
-		$gateway->title       = $payment_category['name'];
-		$gateway->description = $payment_category['description'];
+foreach ( apply_filters( 'qvickly_payments_available_payment_categories', $payment_categories ) as $payment_category ) {
+	$category_id = "qvickly_payments_{$payment_category['id']}";
 
-		// Make sure the first payment category is chosen by default.
-		if ( false !== strpos( $chosen_gateway->id, 'qvickly_payments' ) || $gateway->chosen ) {
-			$gateway->chosen = false;
-			if ( $gateway->title === $payment_categories[ array_key_first( $payment_categories ) ]['type'] ) {
-				$gateway->chosen = true;
-			}
+	$gateway              = $available_gateways['qvickly_payments'] ?? $available_gateways[ $category_id ];
+	$gateway->id          = $category_id;
+	$gateway->icon        = $payment_category['logo'] ?? null;
+	$gateway->title       = $payment_category['name'];
+	$gateway->description = $payment_category['description'];
+
+	// Make sure the first payment category is chosen by default.
+	if ( false !== strpos( $chosen_gateway->id, 'qvickly_payments' ) || $gateway->chosen ) {
+		$gateway->chosen = false;
+		if ( $gateway->title === $payment_categories[ array_key_first( $payment_categories ) ]['type'] ) {
+			$gateway->chosen = true;
 		}
+	}
 
-		// For "Linear Checkout for WooCommerce by Cartimize" to work, we cannot output any HTML.
-		if ( did_action( 'cartimize_get_payment_methods_html' ) === 0 ) {
-			wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
-		}
+	// For "Linear Checkout for WooCommerce by Cartimize" to work, we cannot output any HTML.
+	if ( did_action( 'cartimize_get_payment_methods_html' ) === 0 ) {
+		wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
 	}
 }
